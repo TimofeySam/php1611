3127636b630094f41b3afff167d8f09db533d562
TimofeySam <timofsamoshkin@gmail.com>
2023-02-05 22:21:18 +0300

    Реализованы страницы login и reg

---
+++ profile.php
@@ -0,0 +1,22 @@
+<!-- Конец меню -->
+<div class="container">
+    <p>Имя: <span id="name"></span></p>
+    <p>Фамиля: <span id="lastname"></span></p>
+    <p>Email: <span id="email"></span></p>
+    <p>ID: <span id="id"></span></p>
+</div>
+<script>
+    let name = document.getElementById('name');
+    let lastname = document.getElementById('lastname');
+    let email = document.getElementById('email');
+    let id = document.getElementById('id');
+    fetch('/php/getUserData.php')
+        .then(function (response){return response.json()})
+        .then(function (result){
+            console.log(result);
+            name.innerText = result.name;
+            lastname.innerText = result.lastname;
+            email.innerText = result.email;
+            id.innerText = result.id;
+        });
+</script>
---
+++ router.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+$requestURI = $_SERVER['REQUEST_URI'];
+$path = explode('/', $requestURI);
+if($path[1]=="reg"){
+    $content = file_get_contents("reg.php");
+}else if ($path[1]=="login"){
+    $content = file_get_contents('login.php');
+}else if($path[1]=="profile"){
+    $content = file_get_contents('profile.php');
+}else if($path[1]=="addArticle"){
+    $content = file_get_contents('addArticle.php');
+}else if($path[1] == ""){}
+else if($path[1] == "article"){
+    $articleId = $path[2]; //  /article/2
+    $mysqli = new mysqli("localhost", "root", "", "php1611");
+    $result = $mysqli->query("SELECT * FROM articles WHERE id = $articleId");
+    $row = $result->fetch_assoc();
+    $content = "<h1>".$row['title']."</h1><div>".$row['content']."</div>".$row['author'];
+}else{
+    echo "Страница не найдена 404";
+}
+require_once('template.php');
